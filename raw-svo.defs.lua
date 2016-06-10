c3cf565210179e13106c485d4ad0a024192dff66
jgh713 <jgh713@live.com>
2016-06-09 21:17:23 -0500

    Custom Lyre Fix
    Updating lyre defup line to regex to capture all lyre shortnames.

--- raw-svo.defs.lua
+++ raw-svo.defs.lua
@@ -291,7 +291,7 @@ defs_data = pl.OrderedMap {}
 
       return true
     end,
-    on = {"You strum a Lasallian lyre, and a prismatic barrier forms around you.", "You deftly shape the wall of light into a barrier surrounding yourself.", "You strum a darkly glowing mandolin, and a prismatic barrier forms around you.", "You strum a glittering lyre of ice, and a prismatic barrier forms around you."},
+    onr = [[^You strum an? .+, and a prismatic barrier forms around you\.$]],
     def = "You are standing within a prismatic barrier.",
     off = {"Your prismatic barrier dissolves into nothing.", "The stream hits your prismatic barrier, shattering it.", "The breath weapon rips apart your prismatic barrier.", "The breath weapon rips through both your shield and prismatic barrier.", "The spear shatters your prismatic barrier."}})
   defs_data:set("speed", { type = "general",
