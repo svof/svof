6f6ae78ad8f7512bd21f2495ceafb5f4061771a9
Nathan Cox <nathan.cox@crystalhelix.com>
2017-06-10 11:12:37 -0700

    Remove trailing character capture from form off

--- raw-svo.defs.lua
+++ raw-svo.defs.lua
@@ -1799,42 +1799,42 @@ defs_data = pl.OrderedMap {}
     onenable = onenable_shikudo,
     def = "You are enacting the Tykonos form.",
     on = "You spin your staff in the opening sequence of the form of Tykonos, snapping into a ready stance.",
-    off = [[^You clumsily transition from the form of \w+ into the form of .+]]
+    off = [[^You clumsily transition from the form of \w+ into the form of]]
   })
   defs_data:set("willow", { 
     type = "shikudo",
     onenable = onenable_shikudo,
     def = "You are enacting the Willows shaken by the Wind form..",
     on = "Twirling your staff, you sink into the calm required for the form of Willows Shaken by the Wind.",
-    off = [[^You clumsily transition from the form of \w+ into the form of .+]]
+    off = [[^You clumsily transition from the form of \w+ into the form of]]
   })
   defs_data:set("rain", { 
     type = "shikudo",
     onenable = onenable_shikudo,
     def = "You are enacting the Willows in Rain Storm form.",
     on = "Dropping into a lower stance, you snap your weapon into an offensive position, tensing your muscles in preparation for the form of Willows in Rain Storm.",
-    off = [[^You clumsily transition from the form of \w+ into the form of .+]]
+    off = [[^You clumsily transition from the form of \w+ into the form of]]
   })
   defs_data:set("oak", { 
     type = "shikudo",
     onenable = onenable_shikudo,
     def = "You are enacting the the Live Oak form.",
     on = "Rising onto the balls of your feet, you prepare to begin the deadly form of the Live Oak.",
-    off = [[^You clumsily transition from the form of \w+ into the form of .+]]
+    off = [[^You clumsily transition from the form of \w+ into the form of]]
   })
   defs_data:set("gaital", { 
     type = "shikudo",
     onenable = onenable_shikudo,
     def = "You are enacting the Gaital form.",
     on = "You let your eyes fall closed and instinct guide you as you flow into the form of Gaital.",
-    off = [[^You clumsily transition from the form of \w+ into the form of .+]]
+    off = [[^You clumsily transition from the form of \w+ into the form of]]
   })
   defs_data:set("maelstrom", { 
     type = "shikudo",
     onenable = onenable_shikudo,
     def = "You are enacting the the Unrelenting Storm form.",
     on = "You allow your kai to flow through you, circulating throughout your limbs and down your weapon in preparation to begin the form of the Unrelenting Storm.",
-    off = [[^You clumsily transition from the form of \w+ into the form of .+]]
+    off = [[^You clumsily transition from the form of \w+ into the form of]]
   })
   defs_data:set("grip", { 
     type = "shikudo",
